d8d66bfa0916158d2d5f05e1575bb9ba27ae20ed
shiorioz <ozws011.tmt@gmail.com>
2023-07-28 09:36:33 +0900

    change: 01

--- src/quiz/01.ts
+++ src/quiz/01.ts
@@ -4,7 +4,8 @@
 // ヒント: const の使い方を調べてみましょう
 
 // ここを変更する
-const a: number = 1;
+// const a: number = 1;
+let a: number = 1;
 
 // ここがエラーにならないようにする
 a++;
@@ -13,19 +14,21 @@ a++;
 const b: number = 2;
 
 // ここの型を修正する
-const c: string = b;
+// const c: string = b;
+const c: string = b.toString();
 
 // Q3: 変数 d の定義を修正して型エラーを直してください
 
 // ここの型を修正する
-const d: string = "hello";
+// const d: string = "hello";
+const d: 'hello' = 'hello';
 
 // ここがエラーにならないようにする
-const e: "hello" = d;
+const e: 'hello' = d;
 
 // ---------- 型推論 (Type Inference) ----------
 
 // Q4: 変数 g の定義を修正して型エラーを直してください
 const f = 1;
 
-const g: /* ここに型を書く */ = f;
+const g: number = f;
